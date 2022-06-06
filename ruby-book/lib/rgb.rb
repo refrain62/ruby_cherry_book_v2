0fe66275045839c4c87bfddef8fb02c772f4437e
nao <nao_h13@hotmail.com>
2022-06-06 09:43:33 +0900

    切り出しを正規表現に変更

--- ruby-book/lib/rgb.rb
+++ ruby-book/lib/rgb.rb
@@ -9,7 +9,7 @@ end
 
 def to_ints( hex )
     # 引数の文字列から3つの16進数を取り出す
-    r, g, b = hex[ 1..2 ], hex[ 3..4 ], hex[ 5..6 ]
+    r, g, b = hex.scan(/\w\w/)
     [ r, g, b ].map do |s|
         s.hex
     end
