788570499ab28f6ee94288b67b9ca2cea66672cf
nao <nao_h13@hotmail.com>
2022-06-06 09:37:43 +0900

    4_6_3まで

--- ruby-book/lib/rgb.rb
+++ ruby-book/lib/rgb.rb
@@ -3,6 +3,20 @@ def to_hex( r, g, b )
     [ r, g, b ].each do |n|
         hex += n.to_s( 16 ).rjust( 2, '0' ) 
     end
-    
+
     hex
+end
+
+def to_ints( hex )
+    # 引数の文字列から3つの16進数を取り出す
+    r = hex[ 1..2 ]
+    g = hex[ 3..4 ]
+    b = hex[ 5..6 ]
+    # 3つの16進数を配列に入れ、ループを回しながら10進数の整数に変換した値を別の配列に詰め込む
+    ints = []
+    [ r, g, b ].each do |s|
+        ints << s.hex
+    end
+    # 結果を返す
+    ints
 end
