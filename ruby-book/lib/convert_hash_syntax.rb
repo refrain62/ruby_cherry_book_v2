2b10c7eb9fb2e985ee7b538959a602085587bb6e
nao <nao_h13@hotmail.com>
2022-06-07 14:15:55 +0900

    仮実装

---
+++ ruby-book/lib/convert_hash_syntax.rb
@@ -0,0 +1,4 @@
+def convert_hash_syntax( old_syntax )
+    # 何も変換しない
+    old_syntax
+end
---
+++ ruby-book/test/convert_hash_syntax_text.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require_relative '../lib/convert_hash_syntax'
+
+class ConvertHashSyntaxText < Minitest::Test
+    def test_convert_hash_syntax
+        assert_equal '{}', convert_hash_syntax('{}')
+    end
+end
+
